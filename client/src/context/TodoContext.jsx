20d8de291f19f2cc28a03d5debcc35ce51b2c70f
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-16 14:22:19 +0530

    Update backend URL to production server

--- client/src/context/TodoContext.jsx
+++ client/src/context/TodoContext.jsx
@@ -5,7 +5,7 @@ export const TodoContext = createContext({});
 
 const MyTodoContext = ({children}) => {
 
-  const backendUrl = "http://localhost:4000/api";
+  const backendUrl = "https://todo-app-backend-1dd3.onrender.com/api";
 
   const navigate = useNavigate();
 
